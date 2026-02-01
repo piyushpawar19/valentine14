658cb63aebc66cae8aca45617c79942d7bd999b7
omi0014 <35699949+omi0014@users.noreply.github.com>
2026-02-02 01:59:47 +0530

    Create ValentineProposal.js

---
+++ ValentineProposal.js
@@ -0,0 +1,19 @@
+import { LightningElement,wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+export default class ValentineProposal extends LightningElement {
+    opened = false;
+    sheSaidYes = false;
+    name = 'Saniya';
+    openEnvelope() {
+        this.opened = true;
+    }
+     @wire(CurrentPageReference)
+    getPageReference(pageRef) {
+        if (pageRef && pageRef.state && pageRef.state.name) {
+            this.name = decodeURIComponent(pageRef.state.name);
+        }
+    }
+    handleYes() {
+        this.sheSaidYes = true;
+    }
+}
